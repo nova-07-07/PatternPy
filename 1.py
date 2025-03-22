bffc0edf42bce30239473ad5781e63a574f455df
nova-07-07 <nova234607ns@gmail.com>
2025-03-21 23:24:00 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
-for row in range(8):
-    for col in range(9):
+for row in range(7):
+    for col in range(8):
         if (row == 0 and col % 3 != 0) or (row == 1 and col % 3 == 0) or (row - col == 2) or (row + col == 8):
             print("*", end=" ")
         else:
